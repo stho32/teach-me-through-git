03237d101e89aab12e51149a5267090945480306
stho32 <stho32github@gmail.com>
2023-03-10 22:13:04 +0100

    first draft complete

--- src/gitcommands.rs
+++ src/gitcommands.rs
@@ -19,3 +19,46 @@ pub fn get_commit_count(path: &str) -> Option<u16> {
         Err(_) => { None }
     }
 }
+
+pub fn get_nth_commithash(n : u16) -> Option<String> {
+    // Get the commit hash of the nth commit
+    let output = Command::new("git")
+        .args(&["rev-list", "--max-parents=0", "--skip", &(n.to_string()), "--max-count=1"])
+        .output()
+        .expect("Failed to get commit hash");
+
+    if !output.status.success() {
+        eprintln!("{:?}", output);
+        return None;
+    }
+
+    let commit_hash = String::from_utf8_lossy(&output.stdout).trim().to_string();
+    return Some(commit_hash);
+}
+
+pub fn checkout_commit(commit_hash: &String) {
+    let output = Command::new("git")
+        .args(&["checkout", &commit_hash])
+        .output()
+        .expect("Failed to checkout commit");
+
+    if !output.status.success() {
+        eprintln!("Failed to checkout commit: {:?}", output);
+        return;
+    }
+}
+
+pub fn get_commit_info(commit_hash: &str) -> Result<String, String> {
+    let output = Command::new("git")
+        .args(&["show", commit_hash])
+        .output()
+        .map_err(|e| format!("Failed to get commit information: {}", e))?;
+
+    if !output.status.success() {
+        return Err(format!("Failed to get commit information: {:?}", output));
+    }
+
+    let commit_info = String::from_utf8_lossy(&output.stdout).to_string();
+
+    Ok(commit_info)
+}
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ mod cmdargs;
 mod gitcommands;
 
 use std::io;
+use std::process::{Command, exit};
 use cmdargs::get_path;
 
 #[derive(Clone, Copy)]
@@ -25,23 +26,37 @@ fn get_next_status(status: CommitStatus) -> CommitStatus {
     let buffer = buffer.trim();
 
     if buffer == String::from("+") {
-        return CommitStatus {
-            number_of_commits : status.number_of_commits,
-            current_position : status.current_position + 1
-        }
+        return next_commit(status);
     }
 
     if buffer == String::from("-") {
-        return CommitStatus {
-            number_of_commits : status.number_of_commits,
-            current_position : status.current_position - 1
-        }
+        return previous_commit(status);
     }
 
+    return status;
+}
+
+fn previous_commit(status: CommitStatus) -> CommitStatus {
     return CommitStatus {
-        number_of_commits : status.number_of_commits,
-        current_position : status.current_position
-    };
+        number_of_commits: status.number_of_commits,
+        current_position: status.current_position - 1
+    }
+}
+
+fn next_commit(status: CommitStatus) -> CommitStatus {
+    return CommitStatus {
+        number_of_commits: status.number_of_commits,
+        current_position: status.current_position + 1
+    }
+}
+
+fn enter_directory(path: &String) -> bool {
+    let output = Command::new("cd")
+        .arg(path)
+        .output()
+        .expect("Failed to change directory");
+
+    return output.status.success();
 }
 
 fn main() {
@@ -55,8 +70,23 @@ fn main() {
         current_position: 1
     };
 
+    if !enter_directory(&path) {
+        println!("Could not change into repository directory.");
+        exit(0);
+    }
+
     loop {
         print_status(status);
+
+        let commithash = gitcommands::get_nth_commithash(status.current_position)
+            .expect("Could not get commit hash.");
+
+        gitcommands::checkout_commit(&commithash);
+        let info = gitcommands::get_commit_info(&commithash);
+
+        print!("{:?}", info);
+
         status = get_next_status(status);
     }
 }
+
